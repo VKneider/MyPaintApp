c92b10b425688eaecff11aef706112643224d532
Victor Kneider <victorkneider@gmail.com>
2022-12-21 01:50:12 -0400

    feat: adjust to web page canvas on mobile

--- main.js
+++ main.js
@@ -91,8 +91,8 @@ function init(){
     CANVAS.addEventListener("touchmove", touchHandler, true);
     CANVAS.addEventListener("touchend", touchHandler, true);
     CANVAS.addEventListener("touchcancel", touchHandler, true);    
-    CANVAS.width = window.innerWidth*0.9;
-    CANVAS.height = window.innerHeight * 0.7;
+    CANVAS.width = window.innerWidth*0.935;
+    CANVAS.height = window.innerHeight * 0.695;
     BACKGROUND_CANVAS.width=CANVAS.width;
     BACKGROUND_CANVAS.height=CANVAS.height;
 
